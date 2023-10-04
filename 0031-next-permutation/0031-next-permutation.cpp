20612065ad97b28bf154217af2bd639b7f771026
Anurag Banerjee <anuragbnrj@gmail.com>
2023-10-03 19:35:56 -0700

    Time: 3 ms (65.82%), Space: 12.5 MB (13.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,8 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
+        int len = nums.size();
 
-        next_permutation(nums.begin(), nums.end());
-        
+        int idx = -1;
+        for (int i = len - 2; i >= 0; i--) {
+            int curr = nums[i];
+            int next = nums[i + 1];
+
+            if (curr < next) {
+                idx = i;
+                break;
+            } 
+        }
+
+        if (idx == -1) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for (int i = len - 1; i >= 0; i--) {
+            if (nums[i] > nums[idx]) {
+                swap(nums[i], nums[idx]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + idx + 1, nums.end());
     }
 };
