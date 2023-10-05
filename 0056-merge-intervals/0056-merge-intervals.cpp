ea9116c86507e76661ef9ac90f731ce86585d713
Anurag Banerjee <anuragbnrj@gmail.com>
2023-10-04 19:36:40 -0700

    Time: 37 ms (18.7%), Space: 19.5 MB (35.04%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int len = intervals.size();
-        vector<pair<int, int>> arr(len);
+        sort(intervals.begin(), intervals.end());
 
         vector<vector<int>> res;
-
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < intervals.size(); i++) {
             int beg = intervals[i][0];
             int end = intervals[i][1];
 
-            arr[i] = make_pair(beg, end);
-        }
-
-        sort(arr.begin(), arr.end());
-
-        for (int i = 0; i < len; i++) {
-            int beg = arr[i].first;
-            int end = arr[i].second;
-
-            for (int j = i + 1; j < len && arr[j].first <= end; j++) {
-                i++;
-                end = max(end, arr[j].second);
+            if (res.size() != 0) {
+                if (res.back()[1] >= beg) {
+                    res.back()[1] = max(res.back()[1], end);
+                } else {
+                    res.push_back({beg, end});
+                }
+            } else {
+                res.push_back({beg, end});
             }
-
-            res.push_back({beg, end});
         }
 
         return res;
-        
     }
 };
