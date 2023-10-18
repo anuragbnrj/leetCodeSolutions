ef5d34e6c481677d896a68c1622a011317be752a
Anurag Banerjee <anuragbnrj@gmail.com>
2023-10-18 00:22:58 -0700

    Time: 7 ms (29.07%), Space: 11.4 MB (19.44%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -3,41 +3,13 @@ public:
     int removeDuplicates(vector<int>& nums) {
         int len = nums.size();
 
-        int k = 0;
+        int k = 0; 
         for (int i = 0; i < len; i++) {
-            // int curr = nums[i];
-            // int ctr = 1;
-            // i++;
-            
-            // while (i < len && nums[i] == curr) {
-            //     ctr++;
-            //     i++;
-            // }
-            // i--;
-
-
-            // if (ctr == 1) {
-                
-            // } else if (ctr == 2) {
-
-            // } else {
-            //     k = k + (ctr - 2);
-            // }
-
-            int ctr = 0;
-            for (int j = i; j < len && (nums[j] == nums[i]); j++) {
-                ctr++;
-                if (ctr > 2) {
-                    nums[j] = 1e9;
-                } else {
-                    k++; 
-                }
+            if (i < 2 || nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
             }
-
-            i = i + ctr - 1;
         }
-
-        sort(nums.begin(), nums.end());
 
         return k;
     }
