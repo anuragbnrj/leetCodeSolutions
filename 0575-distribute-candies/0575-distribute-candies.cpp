d8ade4b1d40429cd75488cfbb8666da04705828d
Anurag Banerjee <anuragbnrj@gmail.com>
2023-10-20 18:23:31 -0700

    Time: 180 ms (83.05%), Space: 102.6 MB (82.83%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         int len = candyType.size();
-        set<int> eaten;
+        unordered_set<int> eaten;
 
         int ctr = 0;
         for (int i = 0; i < len && ctr < len / 2; i++) {
-            if (eaten.count(candyType[i])) {
-
-            } else {
+            if (!eaten.count(candyType[i])) {
                 ctr++;
                 eaten.insert(candyType[i]);
-            }
+            } 
         }
 
         return ctr;
