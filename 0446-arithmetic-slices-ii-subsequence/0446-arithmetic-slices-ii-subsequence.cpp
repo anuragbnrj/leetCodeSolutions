55d39b3e90f957677388295f5fb46fbeb5fe85ac
Anurag Banerjee <anuragbnrj@gmail.com>
2024-01-07 16:27:03 -0700

    Time: 632 ms (46.39%), Space: 135.7 MB (66.11%) - LeetHub

---
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int len = nums.size();
+        int res = 0;
+        vector<map<long long, int>> dp(len);
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                long long diff = nums[i] - nums[j] * 1LL;
+                dp[i][diff] += 1;
+                if(dp[j].count(diff)) {
+                    dp[i][diff] += dp[j][diff];
+                    res = res + dp[j][diff];
+                }
+            }
+        }
+
+        return res;
+    }
+
+// private:
+//     int solve(int idx, vector<int>& nums, int subSz, long long diff, int prev) {
+//         // base case
+//         if (idx == nums.size()) {
+//             if(subSz >= 3) {
+//                 return 1;
+//             } else {
+//                 return 0;
+//             }
+//         }
+
+//         int pick = 0;
+//         if (subSz == 0) {
+//             // subsequence.push_back(nums[idx]);
+//             subSz += 1;
+//             pick = solve(idx + 1, nums, subSz, diff, nums[idx]);
+//             subSz -= 1;
+//             // subsequence.pop_back();
+//         }
+
+//         if (subSz == 1) {
+//             // subsequence.push_back(nums[idx]);
+//             subSz += 1;
+//             pick = solve(idx + 1, nums, subSz, nums[idx] - prev * 1LL, nums[idx]);
+//             subSz -= 1;
+//             // subsequence.pop_back();
+//         }
+
+//         if (subSz >= 2) {
+//             long long currDiff = nums[idx] - prev * 1LL;
+//             if (diff == currDiff) {
+//                 // subsequence.push_back(nums[idx]);
+//                 subSz += 1;
+//                 pick = solve(idx + 1, nums, subSz, currDiff, nums[idx]);
+//                 subSz -= 1;
+//                 // subsequence.pop_back();
+//             }
+//         }
+
+
+//         int notPick = solve(idx + 1, nums, subSz, diff, prev);
+
+//         return pick + notPick;
+//     }
+};
