6eda6a1539cc06ecf2c3bf6caeb51c6808479b18
Anurag Banerjee <anuragbnrj@gmail.com>
2024-02-05 17:16:02 -0700

    Time: 24 ms (85.75%), Space: 24.6 MB (17.01%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int len = strs.size();
+
+        map<string, vector<string>> groups;
+        for (int i = 0; i < len; i++) {
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+
+            groups[temp].push_back(strs[i]);
+        }
+
+        vector<vector<string>> res;
+        for (auto el : groups) {
+            res.push_back(el.second);
+        }
+
+        return res;
+    }
+};
