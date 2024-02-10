36849766df466873702494acc63278c749b2f8c7
Anurag Banerjee <anuragbnrj@gmail.com>
2024-02-09 22:40:39 -0700

    Time: 4 ms (76.2%), Space: 7.5 MB (69.49%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -3,44 +3,32 @@ public:
     int countSubstrings(string s) {
         int len = s.size();
         
-        vector<vector<int>> dp(len, vector<int>(len, -1));
         int res = 0;
+        int left, right;
+        // Even length palindromes:
+        for (int i = 0; i < len - 1; i++) {
+            left = i;
+            right = i + 1;
+
+            while (left >=0 && right < len && s[left] == s[right]) {
+                res++;
+                left -= 1;
+                right += 1;
+            }
+        }
+
+        // Odd length palindromes:
         for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                if (isPalindrome(i, j, s, dp) == 1) {
-                    res++;
-                }
+            left = i;
+            right = i;
+
+            while (left >=0 && right < len && s[left] == s[right]) {
+                res++;
+                left -= 1;
+                right += 1;
             }
         }
         
         return res;
-    }
-
-private:
-    int isPalindrome(int beg, int end, string s, vector<vector<int>> &dp) {
-        if (dp[beg][end] != -1) {
-            return dp[beg][end];
-        }
-
-        if (beg > end) {
-            return 0;
-        }
-
-        if (beg == end) {
-            return dp[beg][end] = 1;
-        }
-
-        if ((end - beg) == 1) {
-            if (s[beg] == s[end]) {
-                return dp[beg][end] = 1;
-            }
-        }
-
-        int res = 0;
-        if (isPalindrome(beg + 1, end - 1, s, dp) == 1 && s[beg] == s[end]) {
-            res = 1;
-        }
-
-        return dp[beg][end] = res;
     }
 };
