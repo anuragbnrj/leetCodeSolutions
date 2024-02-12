4d35f3eed60d0fde14588c00a8707f06fb6f0ddb
Anurag Banerjee <anuragbnrj@gmail.com>
2024-02-12 13:12:37 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -2,23 +2,34 @@ class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
         int len = s.size();
+        vector<int> kmp = getKMPArray(s);
 
-        string sub = "";
+        int last = kmp[len];
 
-        for (int i = 0; i < len - 1; i++) {
-            sub += s[i];
-            string temp = "";
-            while ((int)temp.size() < len) {
-                temp += sub;
-            }
-
-            if (temp == s) {
-                // cout << "i: " << i << " temp: " << temp << endl;
-                return true;
-            }
-        }
+        if (len % (len - last) == 0) {
+            return true;
+        } 
 
         return false;
-        
+    }
+
+private:
+    vector<int> getKMPArray(string s) {
+        int len = s.size();
+
+        vector<int> kmp(len + 1);
+        kmp[0] = -1;
+
+        int i = 0, j = -1;
+        while (i < len) {
+            while (j != -1 && s[i] != s[j]) {
+                j = kmp[j];
+            }
+            j++;
+            i++;
+            kmp[i] = j;
+        }
+
+        return kmp;
     }
 };
