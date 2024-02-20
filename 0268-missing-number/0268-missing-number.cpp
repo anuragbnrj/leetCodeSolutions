1def012e263996fb42cd4407397508e221eda2a8
Anurag Banerjee <anuragbnrj@gmail.com>
2024-02-19 19:42:24 -0700

    Time: 12 ms (74.59%), Space: 20.5 MB (18.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,11 +3,7 @@ public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
         int requiredSum = (n * (n + 1)) / 2;
-        int actualSum = 0;
-        for (int i = 0; i < n; i++) {
-            actualSum += nums[i];
-        }
-        
-        return requiredSum - actualSum;
+         
+        return requiredSum - accumulate(nums.begin(), nums.end(), 0);
     }
 };
