bb9f35e8ac5d07e5b1d9967ce784011368ad574f
Anurag Banerjee <anuragbnrj@gmail.com>
2024-02-20 17:38:39 -0700

    Time: 8 ms (53.91%), Space: 8.8 MB (46.79%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-        vector<int> lbits(31, 0);
-        vector<int> rbits(31, 0);
-        for (int bit = 0; bit < 31; bit++) {
-            lbits[bit] = ((left >> bit) & 1);
-            rbits[bit] = ((right >> bit) & 1);
-        }
-
-        vector<int> res(31, 0);
-        for (int bit = 0; bit < 31; bit++) {
-            if (lbits[bit] == 1 && rbits[bit] == 1) {
-                if ((right - left) > pow(2, bit)) {
-                    res[bit] = 0;
-                } else {
-                    res[bit] = 1;
-                }
-            } else {
-                res[bit] = 0;
-            }
-        }
-
         long long ans = 0;
         for (int bit = 0; bit < 31; bit++) {
-            // cout << res[bit] << " ";
-            ans += ((1 << bit) * res[bit]);
+            if (((left >> bit) & 1) == 1 && ((right >> bit) & 1) == 1) {
+                if ((right - left) > pow(2, bit)) {
+                    
+                } else {
+                    ans += (1 << bit);
+                }
+            } else {
+                
+            }
         }
 
         return ans;
