5d8ff7d4272f0879b0faff1edf73ef41246eee3f
Anurag Banerjee <anuragbnrj@gmail.com>
2024-02-24 02:01:57 -0700

    Time: 502 ms (60.25%), Space: 187.2 MB (43.44%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -2,56 +2,89 @@ class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
         int len = meetings.size();
-        vector<vector<pair<int, int>>> graph(n);
-        for (int i = 0; i < len; i++) {
-            int u = meetings[i][0];
-            int v = meetings[i][1];
-            int wt = meetings[i][2];
+        sort(meetings.begin(), meetings.end(), cmp);
 
-            graph[u].push_back({v, wt});
-            graph[v].push_back({u, wt});
-        }
+        initializeDsu(n);
+        unionByRank(0, firstPerson);
+        for (int i = 0; i < len; ) {
+            int currTime = meetings[i][2];
+            
+            unordered_set<int> currNodes;
+            while (i < len && meetings[i][2] == currTime) {
+                int u = meetings[i][0];
+                int v = meetings[i][1];
+                unionByRank(u, v);
+                
+                currNodes.insert(u);
+                currNodes.insert(v);
 
-        graph[0].push_back({firstPerson, 0});
-        graph[firstPerson].push_back({0, 0});
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
-        pq.push({0, 0});
-
-        vector<bool> hasSecret(n, false);
-        vector<bool> visited(n, false);
-        while (!pq.empty()) {
-            auto top = pq.top();
-            pq.pop();
-            int curr = top.second;
-            int currTime = top.first;
-
-            if (visited[curr]) {
-                continue;
+                i++;
             }
+            
 
-            visited[curr] = true;
-            hasSecret[curr] = true;
-
-            for (auto el : graph[curr]) {
-                int neighbour = el.first;
-                int time = el.second;
-                
-                
-                if (!visited[neighbour] && currTime <= time) {
-                    // cout<< curr << "\t" << neighbour << "\t" << time << endl;
-                    pq.push({time, neighbour});
+            for (const auto &node : currNodes) {
+                if (findParent(node) != findParent(0)) {
+                    resetParent(node);
                 }
             }
         }
 
         vector<int> res;
         for (int i = 0; i < n; i++) {
-            if (hasSecret[i]) {
+            if (findParent(i) == findParent(0)) {
                 res.push_back(i);
             }
         }
 
         return res;
+    }
+
+private:
+    vector<int> parent, rank;
+
+    void initializeDsu(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        for (int i = 0; i < n + 1; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int node) {
+        if (parent[node] == node) {
+            return parent[node];
+        }
+
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionByRank(int x, int y) {
+        int parX = findParent(x);
+        int parY = findParent(y);
+
+        if (parX == parY) {
+            return;
+        }
+
+        if (rank[parX] == rank[parY]) {
+            parent[parX] = parY;
+            rank[parY]++;
+        } else if (rank[parX] < rank[parY]) {
+            parent[parX] = parY;
+        } else {
+            parent[parY] = parX;
+        }
+    }
+
+    void resetParent(int node) {
+        parent[node] = node;
+    }
+
+    static bool cmp(const vector<int> &x, const vector<int> &y) {
+        if (x[2] == y[2]) {
+            return x[0] < y[0];
+        } else {
+            return x[2] < y[2];
+        }
     }
 };
