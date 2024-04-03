2f8632b0fb6bdfe18ee1d2ba2db348e136ff45bf
Anurag Banerjee <anuragbnrj@gmail.com>
2024-04-02 17:21:52 -0700

    Time: 740 ms (49.7%), Space: 11.8 MB (18.51%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,53 +3,55 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int rows = board.size();
         int cols = board[0].size();
-
-        bool res = false;
+        
+        bool exists = false;
         vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                res = res || solve(r, c, 0, visited, board, word);
+        for (int r = 0; r < rows && !exists; r++) {
+            for (int c = 0; c < cols && !exists; c++) {
+                exists = exists || isPresent(board, r, c, word, 0, visited, rows, cols);
             }
         }
 
-        return res;
+        return exists;
     }
 
 private:
-    bool solve(int r, int c, int idx, vector<vector<bool>> &visited, vector<vector<char>> &board, string word) {
-        if ((idx == word.size() - 1) && board[r][c] == word[idx]) {
+    bool isPresent(vector<vector<char>> &board, int r, int c, string word, int idx, 
+    vector<vector<bool>> &visited, int rows, int cols) {
+        if (idx == word.size() - 1 && board[r][c] == word[idx]) {
             return true;
         }
 
-        int rows = board.size();
-        int cols = board[0].size();
+        if (board[r][c] != word[idx]) {
+            return false;
+        }
+
+        visited[r][c] = true;
 
         int dr[] = {-1, 0, 0, 1};
         int dc[] = {0, -1, 1, 0};
 
         bool res = false;
-        if(board[r][c] == word[idx]) {
-            visited[r][c] = true;
-            for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
 
-                if (isValidCell(nr, nc, rows, cols) && !visited[nr][nc]) {
-                    res = res || solve(nr, nc, idx + 1, visited, board, word);
-                }
+            if (isOk(nr, nc, rows, cols) && !visited[nr][nc]) {
+                res = res || isPresent(board, nr, nc, word, idx + 1, visited, rows, cols);
             }
-            visited[r][c] = false;
         }
+
+        visited[r][c] = false;
 
         return res;
     }
 
-    bool isValidCell(int r, int c, int rows, int cols) {
-        if (r < 0 || rows - 1 < r) {
+    bool isOk(int r, int c, int rows, int cols) {
+        if (r < 0 || rows <= r) {
             return false;
         }
 
-        if (c < 0 || cols - 1 < c) {
+        if (c < 0 || cols <= c) {
             return false;
         }
 
