6de1371169944e8570fd7cbf5f4596990fd3f802
Anurag Banerjee <anuragbnrj@gmail.com>
2024-04-21 19:53:24 -0700

    Time: 470 ms (12.82%), Space: 40.2 MB (53.65%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.cpp
+++ 0752-open-the-lock/0752-open-the-lock.cpp
@@ -1,50 +1,54 @@
 class Solution {
 public:
-    int res = INT_MAX;
+    
     int openLock(vector<string>& deadends, string target) {
         set<string> dead(deadends.begin(), deadends.end());
 
         string curr = "0000";
-
-        solve(dead, target, curr, 0, 0);
-
-        return (res == INT_MAX ? -1 : res);
-    }
-
-private:
-    void solve(set<string> &dead, string target, string curr, int idx, int moves) {
         if (dead.find(curr) != dead.end()) {
-            return;
+            return -1;
         }
 
-        if (curr == target) {
-            res = min(res, moves);
-            return;
+        queue<string> q;
+        q.push(curr);
+        set<string> visited;
+        visited.insert(curr);
+
+        int level = 0;
+        for ( ;!q.empty(); level++) {
+            int sz = q.size();
+
+            for (int i = 0; i < sz; i++) {
+                string front = q.front();
+                q.pop();
+
+                if (front == target) {
+                    return level;
+                }
+
+                string curr = front;
+                for (int idx = 0; idx < 4; idx++) {
+                    int digit = curr[idx] - '0';
+
+                    int next = (digit + 1) % 10;
+                    curr[idx] = next + '0';
+                    if (dead.find(curr) == dead.end() && visited.find(curr) == visited.end()) {
+                        q.push(curr);
+                        visited.insert(curr);
+                    }
+
+                    int prev = (((digit - 1) % 10) + 10) % 10;
+                    curr[idx] = prev + '0';
+                    if (dead.find(curr) == dead.end() && visited.find(curr) == visited.end()) {
+                        q.push(curr);
+                        visited.insert(curr);
+                    }
+
+                    curr[idx] = digit + '0';
+                }
+            }
         }
 
-        if (idx >= 4) {
-            return;
-        }
-
-        int digit = curr[idx] - '0';
-
-        solve(dead, target, curr, idx + 1, moves);
-        
-        possible = true;
-        for (int i = 1; i < 10 && possible; i++) {
-            int next = (digit + i) % 10;
-            curr[idx] = next + '0';
-            
-            solve(dead, target, curr, idx + 1, moves + i);
-        }
-
-        possible = true;
-        for (int i = 1; i < 10 && possible; i++) {
-            int prev = (((digit - i) % 10) + 10) % 10;
-            curr[idx] = prev + '0';
-            
-            solve(dead, target, curr, idx + 1, moves + i);
-        }
-
+        return -1;
     }
 };
