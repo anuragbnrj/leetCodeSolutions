661c5e19bccc3b570e075784c070609dd385e8ab
Anurag Banerjee <anuragbnrj@gmail.com>
2024-05-02 03:07:16 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -0,0 +1,35 @@
+typedef long long ll;
+
+class Solution {
+public:
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        ll beg = 0;
+        ll end = 1e12 + 5;
+
+        while (beg < end) {
+            ll mid = beg + (end - beg) / 2;
+
+            // cout << mid << endl;
+
+            if (!check(time, totalTrips, mid)) beg = mid + 1;
+            else end = mid;
+        }
+
+        return beg;        
+    }
+
+private:
+    bool check(vector<int> &time, int totalTrips, ll mid) {
+        int n = time.size();
+        ll trips = 0;
+
+        for (int i = 0; i < n; i++) {
+            trips += (mid / (1LL * time[i]));
+        }
+
+        bool val = (trips >= totalTrips ? true : false);
+
+        // cout << mid << ": " << val << endl;
+        return val;
+    }
+};
