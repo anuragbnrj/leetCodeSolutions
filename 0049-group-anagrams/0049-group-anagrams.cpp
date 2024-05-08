4670042d145bff0934d9540a3576ef304e3c273d
Anurag Banerjee <anuragbnrj@gmail.com>
2024-05-07 23:46:33 -0700

    Time: 27 ms (63.2%), Space: 24.6 MB (62.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int len = strs.size();
 
-        unordered_map<string, vector<string>> groups;
+        map<string, vector<string>> groups;
         for (int i = 0; i < len; i++) {
             string temp = strs[i];
-            ranges::sort(temp);
+            sort(temp.begin(), temp.end());
 
             groups[temp].push_back(strs[i]);
         }
