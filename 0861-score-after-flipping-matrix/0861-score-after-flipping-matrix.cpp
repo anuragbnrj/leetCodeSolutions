39e700b8b8c9143f09c033f4cf530ffbc1c7ae02
Anurag Banerjee <anuragbnrj@gmail.com>
2024-05-13 11:36:03 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        for (int r = 0; r < rows; r++) {
+            if (grid[r][0] == 0) {
+                for (int c = 0; c < cols; c++) {
+                    grid[r][c] = 1 - grid[r][c];
+                }
+            }
+        }
+
+        for (int c = 0; c < cols; c++) {
+            int zeros = 0;
+            for (int r = 0; r < rows; r++) {
+                if (grid[r][c] == 0) {
+                    zeros++;
+                }
+            }
+
+            if (zeros > (rows / 2)) {
+                for (int r = 0; r < rows; r++) {
+                    grid[r][c] = 1 - grid[r][c];
+                }
+            }
+        }
+
+        // for (int r = 0; r < rows; r++) {
+        //     for (int c= 0; c < cols; c++) {
+        //         cout << grid[r][c] << "\t";
+        //     }
+        //     cout << endl;
+        // }
+
+        int res = 0;
+        for (int r = 0; r < rows; r++) {
+            int curr = 0;
+            for (int c = cols - 1; c >= 0; c--) {
+                curr = (curr * 2) + grid[r][c];
+            }
+            res += curr;
+        }
+
+        return res;
+        
+
+    }
+};
