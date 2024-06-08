c96a13f31c18e087196637face3580896906ab12
Anurag Banerjee <anuragbnrj@gmail.com>
2024-06-07 19:12:11 -0700

    Time: 229 ms (19.11%), Space: 135.2 MB (14.02%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,42 +3,20 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
 
-        if (n == 1) return false;
-
-        vector<int> temp(nums.begin(), nums.end());
-
+        map<int, int> location;
+        location[0] = -1;
+        int currSum = 0;
         for (int i = 0; i < n; i++) {
-            temp[i] = temp[i] % k;
-        }
+            currSum += nums[i];
+            int rem = currSum % k;
 
-        bool res = false;
-        res = res || isPossible(temp, 0) || isPossible(temp, k) || isPossible(temp, 2LL * k);
-
-        return res;
-    }
-
-private:
-    bool isPossible(vector<int> &temp, long long k) {
-        int n = temp.size();
-        long long currSum = 0;
-        for (int beg = 0, end = 0; end < n; end++) {
-            // cout << "beg: " << beg << ", end: " << end << ", currSum: " << currSum << endl;
-
-            currSum += temp[end];
-
-            if (currSum == k && end - beg + 1 >= 2) {
-                return true;
-            }
-
-            while (currSum > k && beg <= end - 1) {
-                currSum -= temp[beg];
-                beg++;
-
-                if (currSum == k && end - beg + 1 >= 2) {
+            if (location.contains(rem)) {
+                if (i - location[rem] >= 2) {
                     return true;
                 }
+            } else {
+                location[rem] = i;
             }
-
         }
 
         return false;
