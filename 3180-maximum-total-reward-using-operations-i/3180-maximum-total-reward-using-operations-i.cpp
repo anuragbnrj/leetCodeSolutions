24c86e1330cdd76f207eb16b4c34e75e146ad571
Anurag Banerjee <anuragbnrj@gmail.com>
2024-06-09 14:45:27 -0700

    Time: 2873 ms (20%), Space: 347.4 MB (60%) - LeetHub

---
+++ 3180-maximum-total-reward-using-operations-i/3180-maximum-total-reward-using-operations-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rewardValues) {
+        sort(rewardValues.begin(), rewardValues.end());
+        
+        vector<vector<int>> dp(2005, vector<int>(4005, -1));
+        
+        return solve(0, 0, rewardValues, dp);
+    }
+    
+private:
+    int solve(int idx, int prevSum, vector<int> &val, vector<vector<int>> &dp) {
+        int n = val.size();
+        if (idx == n) {
+            return dp[idx][prevSum] = prevSum;
+        }
+        
+        if (prevSum >= 2*(val[n - 1])) {
+            return dp[idx][prevSum] = prevSum;
+        }
+        
+        if (dp[idx][prevSum] != -1) {
+            return dp[idx][prevSum];
+        }
+        
+        int choose = 0, notChoose = 0;
+        
+        // not choose
+        notChoose = solve(idx + 1, prevSum, val, dp);
+        
+        // choose
+        if (prevSum < val[idx]) {
+            choose = solve(idx + 1, prevSum + val[idx], val, dp);
+        }
+        
+        return dp[idx][prevSum] = max(choose, notChoose);
+    }
+};
