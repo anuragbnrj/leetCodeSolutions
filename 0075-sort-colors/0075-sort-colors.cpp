baf0ac2728f4cd218a526d1593a86ce68049d6e9
Anurag Banerjee <anuragbnrj@gmail.com>
2024-06-11 20:37:33 -0700

    Time: 4 ms (31.04%), Space: 9.8 MB (34.14%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,58 +1,21 @@
-//---- Debugger stdout ---- //
-#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cout<<a[i]<<" "; cout<<endl;
-#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cout<<mat[i][j]<<" ";cout<<endl;}
-#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
-template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
-template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
-template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
-template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
-template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
-template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
-template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
-template <class T> void dbs(string str, T t) {cout << str << " : " << t << "\n";}
-template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cout << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
-template <class T> void prc(T a, T b) {cout << "["; for (T i = a; i != b; ++i) {if (i != a) cout << ", "; cout << *i;} cout << "]\n";}
-//----------------- //
-
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n = nums.size();
         int low = 0, mid = 0, high = n - 1;
 
-        // cout << "Beg: " << "\t";
-        // pr(nums);
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                swap(nums[i], nums[low]);
+        for ( ; mid <= high; ) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
                 low++;
                 mid++;
-            } else if (nums[i] == 1) {
+            } else if (nums[mid] == 1) {
                 mid++;
-            } else if (nums[i] == 2 && i < high) {
-                while (high >= 0 && nums[high] == 2) {
-                    high--;
-                }
-
-                if (high < 0) break;
-
-                if (nums[high] == 0) {
-                    swap(nums[high], nums[i]);
-                    high--;
-
-                    swap(nums[i], nums[low]);
-                    low++;
-                    mid++;
-                } else if (nums[high] == 1) {
-                    swap(nums[high], nums[i]);
-
-                    mid++;
-                }
+            } else if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
+                high--;
             }
-
-            // cout << "i: " << i << "\t";
-            // pr(nums);
+            
         }
     }
 };
