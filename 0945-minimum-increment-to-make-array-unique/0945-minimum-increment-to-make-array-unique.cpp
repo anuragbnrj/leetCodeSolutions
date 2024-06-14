610c7812a7bdf9658d0ccad22a87b1175832d81a
Anurag Banerjee <anuragbnrj@gmail.com>
2024-06-13 23:31:35 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,32 +1,41 @@
-using ll = long long;
+//---- Debugger stdout ---- //
+#define debarr(a,n) cout<<#a<<" : ";for(int i=0;i<n;i++) cout<<a[i]<<" "; cout<<endl;
+#define debmat(mat,row,col) cout<<#mat<<" :\n";for(int i=0;i<row;i++) {for(int j=0;j<col;j++) cout<<mat[i][j]<<" ";cout<<endl;}
+#define pr(...) dbs(#__VA_ARGS__, __VA_ARGS__)
+template <class S, class T>ostream& operator <<(ostream& os, const pair<S, T>& p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <class T>ostream& operator <<(ostream& os, const vector<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const unordered_set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const unordered_map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const set<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T>ostream& operator <<(ostream& os, const multiset<T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class S, class T>ostream& operator <<(ostream& os, const map<S, T>& p) {os << "[ "; for (auto& it : p) os << it << " "; return os << "]";}
+template <class T> void dbs(string str, T t) {cout << str << " : " << t << "\n";}
+template <class T, class... S> void dbs(string str, T t, S... s) {int idx = str.find(','); cout << str.substr(0, idx) << " : " << t << ","; dbs(str.substr(idx + 1), s...);}
+template <class T> void prc(T a, T b) {cout << "["; for (T i = a; i != b; ++i) {if (i != a) cout << ", "; cout << *i;} cout << "]\n";}
+//----------------- //
 
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         int n = nums.size();
 
-        unordered_set<int> isUsed;
+        set<int> isUsed;
         sort(nums.begin(), nums.end());
 
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-
-            int beg = num;
-            int end = INT_MAX;
-
-            while (beg < end) {
-                int mid = beg + ((end - beg) >> 1);
-
-                if (isUsed.find(mid) != isUsed.end()) beg = mid + 1;
-                else end = mid; 
+        for (int i = 0, j = nums[0]; i < n; ) {
+            cout << "i: " << i << ", nums[i]: " << nums[i] << " , j: " << j << endl;
+            pr(isUsed);
+            if (isUsed.find(j) != isUsed.end()) {
+                j++;
+            } else {
+                ans += (j - nums[i]);
+                isUsed.insert(j);
+                i++;
+                if (i < n) j = max(j, nums[i]);
             }
-
-            isUsed.insert(beg);
-            ans += beg - num;
         }
 
-        
         return ans;
     }
 };
