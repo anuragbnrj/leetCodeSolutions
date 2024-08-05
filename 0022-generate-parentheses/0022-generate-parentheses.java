cd8d0e9ae52b6c4274bce4995bbd18d0f6bef86f
Anurag Banerjee <anuragbnrj@gmail.com>
2024-08-05 15:41:36 -0700

    Time: 0 ms (100%), Space: 43.1 MB (48.59%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,48 +2,27 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
 
-        solve(0, n, new StringBuilder(), res);
+        solve(n, n, new StringBuilder(), res);
         return res;
     }
 
-    private void solve(int idx, int n, StringBuilder currStr, List<String> res) {
+    private void solve(int l, int r, StringBuilder currStr, List<String> res) {
         // System.out.println("idx: " + idx + ", currStr: " + currStr);
-        if (currStr.length() == (2 * n)) {
-            if (isValid(currStr)) {
-                String str = new String(currStr.toString());
-                res.add(str);
-            }
+        if (l == 0 && r == 0) {
+            res.add(currStr.toString());
             return;
         }
 
-        currStr.append('(');
-        solve(idx + 1, n, currStr, res);
-        currStr.deleteCharAt(currStr.length() - 1);
-
-        currStr.append(')');
-        solve(idx + 1, n, currStr, res);
-        currStr.deleteCharAt(currStr.length() - 1);
-    }
-
-    private boolean isValid(StringBuilder currStr) {
-        Stack<Character> st = new Stack<>();
-
-        for (int i = 0; i < currStr.length(); i++) {
-            char ch = currStr.charAt(i);
-            if (ch == '(') {
-                st.push(ch);
-            } else {
-                if (st.size() == 0 || st.peek() != '(') {
-                    return false;
-                } else {
-                    st.pop();
-                }
-            }
+        if (l > 0) {
+            currStr.append('(');
+            solve(l - 1, r, currStr, res);
+            currStr.deleteCharAt(currStr.length() - 1);
         }
-
-        if (st.size() > 0) {
-            return false;
+        
+        if (r > l) {
+            currStr.append(')');
+            solve(l, r - 1, currStr, res);
+            currStr.deleteCharAt(currStr.length() - 1);
         }
-        return true;
     }
 }
