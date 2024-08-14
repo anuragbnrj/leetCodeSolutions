a90f81bb91f82803c84a7c5ce89c01ef4eec4c19
Anurag Banerjee <anuragbnrj@gmail.com>
2024-08-13 20:31:59 -0700

    Time: 8 ms (84.85%), Space: 46.6 MB (39.17%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,37 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            return a[0] - b[0];
+        });
+
         int n = intervals.length;
-        List<List<Integer>> tempRes = new ArrayList<>();
-        
         int prevBeg = intervals[0][0];
         int prevEnd = intervals[0][1];
-
+        List<int[]> temp = new ArrayList<>();
         for (int i = 1; i < n; i++) {
             int currBeg = intervals[i][0];
             int currEnd = intervals[i][1];
-            
-            int overlap = Math.max(-1, Math.min(currEnd, prevEnd) - Math.max(currBeg, prevBeg));
 
-            if (overlap == -1) {
-                List<Integer> temp = Arrays.asList(prevBeg, prevEnd);
-                tempRes.add(temp);
-                prevBeg = currBeg;
-                prevEnd = currEnd;
-            } else {
+            int intersection = Math.min(prevEnd, currEnd) - Math.max(prevBeg, currBeg);
+
+            if (intersection >= 0) {
                 prevBeg = Math.min(prevBeg, currBeg);
                 prevEnd = Math.max(prevEnd, currEnd);
-            }
-        }
-        
-        List<Integer> temp = Arrays.asList(prevBeg, prevEnd);
-        tempRes.add(temp);
+            } else {
+                temp.add(new int[]{prevBeg, prevEnd});
 
-        int[][] res = new int[tempRes.size()][2];
-        for (int i = 0; i < tempRes.size(); i++) {
-            res[i][0] = tempRes.get(i).get(0);
-            res[i][1] = tempRes.get(i).get(1);
+                prevBeg = currBeg;
+                prevEnd = currEnd;
+            }  
         }
+        temp.add(new int[]{prevBeg, prevEnd});
 
-        return res;
+        return temp.toArray(new int[temp.size()][]);
     }
 }
