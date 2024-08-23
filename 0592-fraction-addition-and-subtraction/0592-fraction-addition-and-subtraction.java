487f1aada3cc37653cfd65b71a91b80af3db8fcc
Anurag Banerjee <anuragbnrj@gmail.com>
2024-08-22 20:35:36 -0700

    Time: 15 ms (19.73%), Space: 42.8 MB (18.35%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.java
@@ -23,76 +23,37 @@ class Solution {
         // for (String f : fractions) {
         //     System.out.println(f);
         // }
-
-        int n = fractions.size();
-        long[] nums = new long[n];
-        long[] dens = new long[n];
         
-        for (int i = 0; i < n; i++) {
+        int fnum = 0, fden = 1;
+        for (int i = 0; i < fractions.size(); i++) {
             String[] temp = fractions.get(i).split("/");
 
-            long num = Long.parseLong(temp[0]);
-            long den = Long.parseLong(temp[1]);
+            int currNum = Integer.parseInt(temp[0]);
+            int currDen = Integer.parseInt(temp[1]);
 
-            nums[i] = num;
-            dens[i] = den;            
+            fnum = fnum * currDen + currNum * fden;
+            fden = fden * currDen;
+
+            System.out.println("i: " + i + ", fnum: " + fnum + ", fden: " + fden);           
         }
 
-        long lcmm = lcm(dens);
-        long fnum = 0, fden = lcmm;
-        for (int i = 0; i < n; i++) {
-            fnum += nums[i] * (lcmm / dens[i]);
-        }
-        long hcf = gcd(fnum, fden);
-        fnum = fnum / hcf;
-        fden = fden / hcf;
+        
+        int gcd = Math.abs(euclidsGcd(fnum, fden));
+        fnum = fnum / gcd;
+        fden = fden / gcd;
 
-        String ans = Long.toString(fnum) + "/" + Long.toString(fden);
+        String ans = Integer.toString(fnum) + "/" + Integer.toString(fden);
 
         return ans;
     }
 
-    private static long safe_mod(long x, long m){
-        x %= m;
-        if(x<0) x += m;
-        return x;
-    }
-
-    private static long[] inv_gcd(long a, long b){
-        a = safe_mod(a, b);
-        if(a==0) return new long[]{b,0};
-
-        long s=b, t=a;
-        long m0=0, m1=1;
-        while(t>0){
-            long u = s/t;
-            s -= t*u;
-            m0 -= m1*u;
-            long tmp = s; s = t; t = tmp;
-            tmp = m0; m0 = m1; m1 = tmp;
+    private int euclidsGcd(int a, int b) {
+        while (b != 0) {
+            int t = b;
+            b = a % b;
+            a = t;
         }
-        if(m0<0) m0 += b/s;
-        return new long[]{s,m0};
-    }
 
-    public static long gcd(long... a){
-        if(a.length == 0) return 0;
-        long r = java.lang.Math.abs(a[0]);
-        for(int i=1; i<a.length; i++){
-            if(a[i]!=0) {
-                if(r==0) r = java.lang.Math.abs(a[i]);
-                else r = inv_gcd(r, java.lang.Math.abs(a[i]))[0];
-            }
-        }
-        return r;
-    }
-
-    public static long lcm(long... a){
-        if(a.length == 0) return 0;
-        long r = java.lang.Math.abs(a[0]);
-        for(int i=1; i<a.length; i++){
-            r = r / gcd(r,java.lang.Math.abs(a[i])) * java.lang.Math.abs(a[i]);
-        }
-        return r;
+        return a;
     }
 }
