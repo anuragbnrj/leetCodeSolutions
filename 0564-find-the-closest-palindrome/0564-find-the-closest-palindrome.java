fec54f7bcf1abd2ecc3427b41070821e33c46ed3
Anurag Banerjee <anuragbnrj@gmail.com>
2024-08-23 20:48:54 -0700

    Time: 12 ms (5.43%), Space: 45 MB (5.43%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.java
@@ -4,6 +4,9 @@ class Solution {
         long next = findNextPalin(num);
         long prev = findPrevPalin(num);
 
+        System.out.println("next: " + next);
+        System.out.println("prev: " + prev);
+
         if (Math.abs(num - prev) <= Math.abs(next - num)) {
             return Long.toString(prev);
         }
@@ -13,44 +16,42 @@ class Solution {
     private long findPrevPalin(long num) {
         long beg = 0;
         long end = num;
-        long ans = 0L;
 
-        while (beg <= end) {
+        while (beg < end) {
             long mid = beg + (end - beg) / 2;
 
             long palin = convertToPalin(mid);
             if (palin < num) {
-                ans = palin;
                 beg = mid + 1;
             } else {
-                end = mid - 1;
+                end = mid;
             }
         }
 
-        return ans;
+        return convertToPalin(beg - 1);
     }
 
     private long findNextPalin(long num) {
         long beg = num;
         long end = (long) 1e18;
-        long ans = 0L;
-        while (beg <= end) {
-            long mid = beg + (end - beg) / 2;
 
+        while (beg < end) {
+            long mid = beg + (end - beg) / 2;
             long palin = convertToPalin(mid);
  
-            if (palin > num) {
-                ans = palin;
-                end = mid - 1;
-            } else {
+            if (palin <= num) {
                 beg = mid + 1;
+            } else {
+                end = mid;
             }
         }
 
-        return ans;
+        return convertToPalin(beg);
     }
 
     private long convertToPalin(long num) {
+        // System.out.println(num);
+        
         char[] arr = Long.toString(num).toCharArray();
         int n = arr.length;
         int l = (n - 1) / 2;
