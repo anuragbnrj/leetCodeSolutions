d9871a1476d40c0eb6f60b2c6c0a6cfa82240190
Anurag Banerjee <anuragbnrj@gmail.com>
2024-10-02 19:40:43 -0700

    Time: 114 ms (5.63%), Space: 58.7 MB (5.16%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+        int len = nums.length;
+        int tot = 0;
+        for (int i = 0; i < len; i++) {
+            tot = (tot + nums[i]) % p;
+            nums[i] = nums[i] % p; 
+        }
+        int modReq = tot % p;
+
+        if (modReq == 0) return 0;
+
+        int minLen = (int) 1e9;
+        TreeMap<Integer, Integer> prevModOccur = new TreeMap<>();
+        prevModOccur.put(0, -1);
+        int currSumMod = 0;
+        for (int i = 0; i < len; i++) {
+            currSumMod = (currSumMod + nums[i]) % p;
+            int prevMod = (((currSumMod - modReq) % p) + p) % p;
+
+            if (prevModOccur.containsKey(prevMod)) {
+                minLen = Math.min(minLen, (i - prevModOccur.get(prevMod)));
+            }
+
+            prevModOccur.put(currSumMod, i);
+        }
+
+        if (minLen == len) {
+            return -1;
+        }
+
+        return minLen;
+    }
+}
