0da59fa0e463f38398a38c2e9d6a16c8ecc02ac3
Anurag Banerjee <anuragbnrj@gmail.com>
2024-10-04 17:36:31 -0700

    Time: 7 ms (72.48%), Space: 42.6 MB (71.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,48 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-
-        if (n2 < n1) {
-            return false;
-        }
-
         int[] freq1 = new int[26];
-        for (int i = 0; i < n1; i++) {
-            char ch = s1.charAt(i);
-
+        for (char ch : s1.toCharArray()) {
             freq1[ch - 'a'] += 1;
         }
 
         int[] freq2 = new int[26];
-        for (int i = 0; i < n1 - 1; i++) {
-            char ch = s2.charAt(i);
-
-            freq2[ch - 'a'] += 1;
-        }
-
-        for (int right = n1 - 1, left = 0; right < n2; right++) {
-            char ch = s2.charAt(right);
+        for (int beg = 0, end = 0; end < s2.length(); end++) {
+            char ch = s2.charAt(end);
             freq2[ch - 'a'] += 1;
 
-            boolean ans = true;
-            for (int i = 0; i < 26; i++) {
-                if (freq2[i] < freq1[i]) {
-                    ans = false;
-                    break;
+            while ((end - beg + 1) > s1.length()) {
+                ch = s2.charAt(beg);
+                freq2[ch - 'a'] -= 1;
+                beg += 1;
+            }
+
+            if ((end - beg + 1) == s1.length()) {
+                if (compare(freq1, freq2)) {
+                    return true;
                 }
             }
-
-            if (ans == true) {
-                return ans;
-            }
-
-            ch = s2.charAt(left);
-            freq2[ch - 'a'] -= 1;
-            left += 1;
         }
 
         return false;
-        
+    }
+
+    private boolean compare(int[] freq1, int[] freq2) {
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
