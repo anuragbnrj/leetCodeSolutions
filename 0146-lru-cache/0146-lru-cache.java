bb3514f4042a8a8eac31d7069f09e1555c8c65c6
Anurag Banerjee <anuragbnrj@gmail.com>
2024-10-05 17:55:42 -0700

    Time: 45 ms (71.25%), Space: 112.4 MB (83.29%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,58 +1,74 @@
 class LRUCache {
+
     int capacity;
-    Map<Integer, Node> dic;
+    int currSize;
+    HashMap<Integer, Node> nodeMap;
     Node head;
     Node tail;
 
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-        dic = new HashMap<>();
+    public LRUCache(int c) {
+        capacity = c;
+        currSize = 0;
+        nodeMap = new HashMap<>();
+
         head = new Node(-1, -1);
         tail = new Node(-1, -1);
-
         head.next = tail;
         tail.prev = head;
     }
     
     public int get(int key) {
-        if (!dic.containsKey(key)) {
+        if (!nodeMap.containsKey(key)) {
             return -1;
         }
 
-        Node node = dic.get(key);
-        remove(node);
-        add(node);
+        Node node = nodeMap.get(key);
+        removeNode(node);
+        addToHead(node);
+
         return node.val;
     }
-    
+
     public void put(int key, int value) {
-        if (dic.containsKey(key)) {
-            Node nodeToBeRemoved = dic.get(key);
-            dic.remove(nodeToBeRemoved.key);
-            remove(nodeToBeRemoved);
-        }
+        if (nodeMap.containsKey(key)) {
+            Node node = nodeMap.get(key);
 
-        Node newNode = new Node(key, value);
-        add(newNode);
-        dic.put(key, newNode);
+            node.val = value;
+            removeNode(node);
+            addToHead(node);
+        } else {
+            Node node = new Node(key, value);
+            nodeMap.put(key, node);
+            addToHead(node);
+            currSize += 1;
 
-        if (dic.size() > capacity) {
-            Node nodeToBeRemoved = tail.prev;
-            dic.remove(nodeToBeRemoved.key);
-            remove(tail.prev);
+            if (currSize > capacity) {
+                int removeKey = removeFromTail();
+                nodeMap.remove(removeKey);
+            }
         }
     }
 
-    private void remove(Node node) {
+    private void removeNode(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
+
+        node.next = null;
+        node.prev = null;
     }
 
-    private void add(Node node) {
+    private void addToHead(Node node) {
         node.next = head.next;
-        node.prev = head;
+        head.next.prev = node;
         head.next = node;
-        node.next.prev = node;
+        node.prev = head;
+    }
+
+    private int removeFromTail() {
+        Node node = tail.prev;
+        removeNode(node);
+
+        return node.key;
     }
 
     class Node {
@@ -61,15 +77,9 @@ class LRUCache {
         Node prev;
         Node next;
 
-        public Node() {
-            val = 0;
-            prev = null;
-            next = null;
-        }
-
-        public Node(int key, int val) {
-            this.key = key;
-            this.val = val;
+        public Node(int k, int v) {
+            key = k;
+            val = v;
             prev = null;
             next = null;
         }
