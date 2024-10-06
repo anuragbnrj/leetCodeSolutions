91dec54efb45b06c1dc14f6bc9b88c5436913c40
Anurag Banerjee <anuragbnrj@gmail.com>
2024-10-05 17:56:40 -0700

    Time: 44 ms (78.7%), Space: 112.3 MB (84.01%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,88 +1,27 @@
 class LRUCache {
+    private static int max_capacity;
+    Map<Integer, Integer> kvStore;
 
-    int capacity;
-    int currSize;
-    HashMap<Integer, Node> nodeMap;
-    Node head;
-    Node tail;
-
-    public LRUCache(int c) {
-        capacity = c;
-        currSize = 0;
-        nodeMap = new HashMap<>();
-
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head.next = tail;
-        tail.prev = head;
+    public LRUCache(int capacity) {
+        max_capacity = capacity;
+        this.kvStore = new LinkedHashMap<>(capacity, 1.0F, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > max_capacity;
+            }
+        };    
     }
     
     public int get(int key) {
-        if (!nodeMap.containsKey(key)) {
+        if (!kvStore.containsKey(key)) {
             return -1;
         }
 
-        Node node = nodeMap.get(key);
-        removeNode(node);
-        addToHead(node);
-
-        return node.val;
+        return kvStore.get(key);
     }
-
+    
     public void put(int key, int value) {
-        if (nodeMap.containsKey(key)) {
-            Node node = nodeMap.get(key);
-
-            node.val = value;
-            removeNode(node);
-            addToHead(node);
-        } else {
-            Node node = new Node(key, value);
-            nodeMap.put(key, node);
-            addToHead(node);
-            currSize += 1;
-
-            if (currSize > capacity) {
-                int removeKey = removeFromTail();
-                nodeMap.remove(removeKey);
-            }
-        }
-    }
-
-    private void removeNode(Node node) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-
-        node.next = null;
-        node.prev = null;
-    }
-
-    private void addToHead(Node node) {
-        node.next = head.next;
-        head.next.prev = node;
-        head.next = node;
-        node.prev = head;
-    }
-
-    private int removeFromTail() {
-        Node node = tail.prev;
-        removeNode(node);
-
-        return node.key;
-    }
-
-    class Node {
-        int key;
-        int val;
-        Node prev;
-        Node next;
-
-        public Node(int k, int v) {
-            key = k;
-            val = v;
-            prev = null;
-            next = null;
-        }
+        kvStore.put(key, value);
     }
 }
 
