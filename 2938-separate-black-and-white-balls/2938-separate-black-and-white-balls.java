763d553aaa7a1b57e203a26527d97a8aab4f65e3
Anurag Banerjee <anuragbnrj@gmail.com>
2024-10-14 18:30:04 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -0,0 +1,22 @@
+class Solution {
+    public long minimumSteps(String s) {
+        int len = s.length();
+        int[] suffixZeroes = new int[len + 1];
+
+        int zeroCount = 0;
+        int ans = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+
+            suffixZeroes[i] = zeroCount;
+
+            if (ch == '0') {
+                zeroCount += 1;
+            } else {
+                ans += suffixZeroes[i];
+            }
+        }
+
+        return ans;
+    }
+}
