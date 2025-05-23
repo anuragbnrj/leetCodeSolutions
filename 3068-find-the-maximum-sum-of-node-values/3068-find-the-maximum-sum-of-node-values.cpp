c771232c5268f4aa5c764da987eee8df857bfa6d
Anurag Banerjee <anuragbnrj@gmail.com>
2025-05-23 00:21:35 -0700

    Time: 244 ms (5.16%), Space: 132.4 MB (29.03%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -10,13 +10,31 @@ public:
             int nval = nums[i] ^ k;
 
             int delta = nval - nums[i];
-            pq.push_back({delta, i});
+            pq.push({delta, i});
         }
 
+        long long res = baseSum;
         while (pq.size() >= 2) {
             auto a = pq.top(); 
             pq.pop();
-            auto b = pq.top(); pq.pop()
+            long long deltaA = a.first;
+
+            auto b = pq.top(); 
+            pq.pop();
+            long long deltaB = b.first;
+
+            long long deltaNet = deltaA + deltaB;
+
+            if (deltaNet <= 0) {
+                break;
+            } else {
+                res += deltaNet;
+                pq.push({-deltaA, a.second});
+                pq.push({-deltaB, b.second});
+            }
+
         }
+
+        return res;
     }
 };
