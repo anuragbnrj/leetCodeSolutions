3a7c6c2e4a0bc5b853cf728a63a483e0023d9d9b
Anurag Banerjee <anuragbnrj@gmail.com>
2025-07-28 00:49:07 -0700

    Time: 9 ms (48.4%), Space: 40.9 MB (94.46%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,33 +1,25 @@
 class Solution {
-
+    
     public int countMaxOrSubsets(int[] nums) {
-        int len = 3 * (int) 1e5 + 1;
-        int[] freq = new int[len];
-
-        rec(0, nums, freq, 0);
-
-        int ans = 0;
-        for (int i = len - 1; i >= 0; i--) {
-            if (freq[i] > 0) {
-                ans = freq[i];
-                break;
-            }
+        int len = nums.length;
+        int maxor = 0;
+        for (int num : nums) {
+            maxor = maxor | num;
         }
 
+        int ans = rec(0, nums, 0, maxor);
         return ans;
     }
 
-    private void rec(int idx, int[] nums, int[] freq, int currOr) {
-        if (idx >= nums.length) {
-            // System.out.println("idx: " + idx + ", currXor: " + currOr);
-            freq[currOr] += 1;
-            return;
+    private int rec(int idx, int[] nums, int curror, int maxor) {
+        if (idx == nums.length) {
+            if (curror == maxor) {
+                return 1;
+            }
+
+            return 0;
         }
 
-        // pick
-        rec(idx + 1, nums, freq, (currOr | nums[idx]));
-
-        // not pick
-        rec(idx + 1, nums, freq, currOr);
+        return rec(idx + 1, nums, curror | nums[idx], maxor) + rec(idx + 1, nums, curror, maxor);
     }
 }
