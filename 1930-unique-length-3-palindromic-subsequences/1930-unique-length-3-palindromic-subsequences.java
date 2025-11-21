9e683aa50af31c63c27c9f3f6f77968c77cd8d54
Anurag Banerjee <anuragbnrj@gmail.com>
2025-11-21 01:41:43 -0700

    Time: 7 ms (100%), Space: 47 MB (23.4%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.java
@@ -1,44 +1,47 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        char[] strarr = s.toCharArray();
-        int len = s.length();
-
-        int[] firstOccurrence = new int[26];
-        Arrays.fill(firstOccurrence, -1);
-        for (int i = 0; i < len; i++) {
-            char ch = strarr[i];
-            if (firstOccurrence[ch - 'a'] == -1) {
-                firstOccurrence[ch - 'a'] = i; 
+        // 1. Find the first and last occurrence of every character
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, -1);
+        
+        for (int i = 0; i < s.length(); i++) {
+            int curr = s.charAt(i) - 'a';
+            if (first[curr] == -1) {
+                first[curr] = i;
             }
+            last[curr] = i;
         }
-
-        int[] lastOccurrence = new int[26];
-        Arrays.fill(lastOccurrence, -1);
-        for (int i = len - 1; i >= 0; i--) {
-            char ch = strarr[i];
-            if (lastOccurrence[ch - 'a'] == -1) {
-                lastOccurrence[ch - 'a'] = i;
-            }
-        }
-
+        
         int ans = 0;
+        
+        // 2. Iterate over each character to use as the "shell" (outer characters)
         for (int i = 0; i < 26; i++) {
-            int left = firstOccurrence[i] + 1;
-            int right = lastOccurrence[i] - 1;
-
-            ans += countUnique(strarr, left, right);
+            // If the character doesn't exist or only exists once, it can't be a shell
+            if (first[i] == -1 || last[i] == first[i]) {
+                continue;
+            }
+            
+            // 3. Find unique characters betweeen the first and last occurrence
+            // Using a boolean array (or HashSet) to count unique chars
+            boolean[] found = new boolean[26];
+            int distinctCount = 0;
+            
+            // Iterate strictly between the first and last index of character 'i'
+            for (int k = first[i] + 1; k < last[i]; k++) {
+                int charIndex = s.charAt(k) - 'a';
+                if (!found[charIndex]) {
+                    found[charIndex] = true;
+                    distinctCount++;
+                    
+                    // Optimization: If we found all 26 types in the middle, we can stop early
+                    if (distinctCount == 26) break;
+                }
+            }
+            
+            ans += distinctCount;
         }
         
         return ans;
-    }
-
-    private int countUnique(char[] strarr, int left, int right) {
-        Set<Character> uniqueChars = new HashSet<>();
-
-        for (int i = left; i <= right; i++) {
-            uniqueChars.add(strarr[i]);
-        }
-
-        return uniqueChars.size();
     }
 }
