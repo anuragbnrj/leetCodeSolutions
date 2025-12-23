807637e5776c4e9d9d1e59707335a94c0986f432
Anurag Banerjee <anuragbnrj@gmail.com>
2025-12-23 01:40:10 -0700

    Time: 228 ms (6%), Space: 188.1 MB (16%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.java
@@ -1,39 +1,33 @@
 class Solution {
     public int maxTwoEvents(int[][] events) {
-        Arrays.sort(events, (a, b) -> {
-            if (a[0] == b[0]) {
-                return Integer.compare(a[1], b[1]);
-            } 
+        Arrays.sort(events, (a, b)-> {
+            if (a[1] == b[1]) {
+                if (a[2] == b[2]) {
+                    return Integer.compare(b[0], a[0]);
+                }
 
-            return Integer.compare(a[0], b[0]);
-        });
-
-        TreeMap<Integer, Integer> maxFromThis = new TreeMap<>();
-        int maxTillNow = 0;
-        for (int i = events.length - 1; i >= 0 ; i--) {
-            int[] event = events[i];
-
-            if (event[2] > maxTillNow) {
-                maxTillNow = event[2];    
+                return Integer.compare(a[2], b[2]);
             }
 
-            maxFromThis.put(event[0], maxTillNow);
-        }
+            return Integer.compare(a[1], b[1]);
+        });
 
+        TreeMap<Integer, Integer> bestEndingAtOrBefore = new TreeMap<>();
+        bestEndingAtOrBefore.put(0, 0);
+        int bestTillNow = 0;
         int ans = 0;
         for (int i = 0; i < events.length; i++) {
             int beg = events[i][0];
             int end = events[i][1];
             int val = events[i][2];
 
-            Integer ceilKey = maxFromThis.ceilingKey(end + 1);
+            int otherEvent = bestEndingAtOrBefore.get(bestEndingAtOrBefore.floorKey(beg - 1));
 
-            int currVal = val;
-            if (ceilKey != null) {
-                currVal += maxFromThis.get(ceilKey);
-            }
+            ans = Math.max(ans, otherEvent + val);
 
-            ans = Math.max(ans, currVal);
+            bestTillNow = Math.max(bestTillNow, val);
+
+            bestEndingAtOrBefore.put(end, Math.max(bestEndingAtOrBefore.getOrDefault(end, 0), bestTillNow));
         }
 
         return ans;
