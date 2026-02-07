5ceae6134c27cf9835add2412e15757c1bea306f
Anurag Banerjee <anuragbnrj@gmail.com>
2026-02-07 14:48:23 -0700

    Time: 45 ms (27.41%), Space: 47.9 MB (45.84%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.java
@@ -1,51 +1,33 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int n = s.length();
-        char[] sarr = s.toCharArray();
-
-        int[] as = new int[n + 2];
-        int[] bs = new int[n + 2];
-
-        for (int i = 1; i <= n; i++) {
-            char ch = sarr[i - 1];
-
-            if (ch == 'b') {
-                bs[i] = bs[i - 1] + 1;
+        int len = s.length();
+        
+        int[] aEnd = new int[len + 1];
+        int[] bEnd = new int[len + 1];
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) == 'a') {
+                aEnd[i] = aEnd[i + 1] + 1;
+                bEnd[i] = bEnd[i + 1];
             } else {
-                bs[i] = bs[i - 1];
+                aEnd[i] = aEnd[i + 1];
+                bEnd[i] = bEnd[i + 1] + 1;
             }
         }
 
-        int acount = 0;
-        for (int i = n; i >= 1; i--) {
-            char ch = sarr[i - 1];
-
-            if (ch == 'a') {
-                as[i] = as[i + 1] + 1;
+        int aBeg = 0;
+        int bBeg = 0;
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            int deletions = 0;
+            if (s.charAt(i) == 'a') {
+                deletions += bBeg + aEnd[i + 1];
+                aBeg += 1;
             } else {
-                as[i] = as[i + 1];
-            }
-        }
-
-        int ans = 0;
-        int bdeleted = 0;
-        for (int i = 1; i <= n; i++) {
-            char ch = sarr[i - 1];
-
-            if (ch == 'a') {
-                if (as[i] < (bs[i] - bdeleted)) {
-                    ans += 1;
-                    System.out.println("a i: " + i);
-                }
+                deletions += bBeg + aEnd[i + 1];
+                bBeg += 1;
             }
 
-            if (ch == 'b') {
-                if (bs[i] <= as[i]) {
-                    ans += 1;
-                    bdeleted += 1;
-                    System.out.println("b i: " + i);
-                }
-            }
+            ans = Math.min(ans, deletions);
         }
 
         return ans;
