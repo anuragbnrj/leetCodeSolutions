4aaaaa31463d1ce07e15552bed6c0502015e2b26
Anurag Banerjee <anuragbnrj@gmail.com>
2026-02-28 02:35:43 -0700

    Time: 1674 ms (5.81%), Space: 46.5 MB (19.77%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.java
@@ -2,36 +2,28 @@ class Solution {
     private static final long MOD = (int) 1e9 + 7;
 
     public int concatenatedBinary(int n) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) {
-            concatenateBinary(sb, i);
-        }
-
         long ans = 0;
-        for (int i = 0; i < sb.length(); i++) {
-            char ch = sb.charAt(i);
+        for (int i = 1; i <= n; i++) {
+            int num = i;
 
-            ans = (ans * 2L) % MOD;
-            ans = (ans + (ch - '0')) % MOD;
+            List<Integer> binary = new ArrayList<>();
+            while (num > 0) {
+                int r = num % 2;
+                if (r == 0) {
+                    binary.add(0);
+                } else {
+                    binary.add(1);
+                }
+
+                num /= 2;
+            }
+
+            for (int j = binary.size() - 1; j >= 0; j--) {
+                ans = (ans * 2L) % MOD;
+                ans = (ans + binary.get(j)) % MOD;
+            }
         }
 
         return (int) ans;
-    }
-
-    private void concatenateBinary(StringBuilder sb, int num) {
-        StringBuilder temp = new StringBuilder();
-        while (num > 0) {
-            int r = num % 2;
-            if (r == 0) {
-                temp.append('0');
-            } else {
-                temp.append('1');
-            }
-
-            num /= 2;
-        }
-
-        temp.reverse();
-        sb.append(temp);
     }
 }
