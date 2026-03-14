d850712a66c849ac916ff6d0fdcddee216fa0e02
Anurag Banerjee <anuragbnrj@gmail.com>
2026-03-13 22:10:29 -0700

    Time: 1 ms (100%), Space: 43.5 MB (63.74%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -6,44 +6,35 @@ class Solution {
         count = 0;
         ans = "";
 
-        backtrack(0, new StringBuilder(), n, k);
+        StringBuilder sb = new StringBuilder();
+        rec('z', sb, n, k);
+
         return ans;
     }
 
-    private void backtrack(int idx, StringBuilder curr, int n, int k) {
-        if (idx == n) {
+    private boolean rec(char prevChar, StringBuilder curr, int n, int k) {
+        if (curr.length() == n) {
             count += 1;
 
             if (count == k) {
                 ans = curr.toString();
-            }
-
-            return;
-        }
-
-        if (idx == 0) {
-            curr.append("a");
-            backtrack(idx + 1, curr, n, k);
-            curr.deleteCharAt(curr.length() - 1);
-
-            curr.append("b");
-            backtrack(idx + 1, curr, n, k);
-            curr.deleteCharAt(curr.length() - 1);
-
-            curr.append("c");
-            backtrack(idx + 1, curr, n, k);
-            curr.deleteCharAt(curr.length() - 1);
-        } else {
-            char prev = curr.charAt(curr.length() - 1);
-
-            char[] arr = {'a', 'b', 'c'};
-            for (int i = 0; i < 3; i++) {
-                if (arr[i] != prev) {
-                    curr.append(arr[i]);
-                    backtrack(idx + 1, curr, n, k);
-                    curr.deleteCharAt(curr.length() - 1);
-                }
+                return true;
+            } else {
+                return false;
             }
         }
+
+        char[] arr = {'a', 'b', 'c'};
+        for (char ch : arr) {
+            if (ch != prevChar) {
+                curr.append(ch);
+                boolean res = rec(ch, curr, n, k);
+                curr.deleteCharAt(curr.length() - 1);
+
+                if (res) return true;
+            }
+        }
+
+        return false;
     }
 }
