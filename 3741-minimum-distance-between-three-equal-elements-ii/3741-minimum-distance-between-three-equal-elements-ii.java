53ee18008dd38ba8eb643d409debfe86c5fb616d
Anurag Banerjee <anuragbnrj@gmail.com>
2026-04-11 04:33:22 -0700

    Time: 101 ms (82.12%), Space: 157.9 MB (100%) - LeetHub

---
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        int ans = 1_000_000_000;
+
+        int[] prevOccur = new int[100005];
+        Arrays.fill(prevOccur, -1);
+
+        int[] prevPrevOccur = new int[100005];
+        Arrays.fill(prevPrevOccur, -1);
+
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (prevPrevOccur[num] == -1 && prevOccur[num] == -1) {
+                prevOccur[num] = i;
+            } else if (prevPrevOccur[num] == -1 && prevOccur[num] != -1) {
+                prevPrevOccur[num] = prevOccur[num];
+                prevOccur[num] = i;
+            } else {
+                ans = Math.min(ans, (i - prevPrevOccur[num]) + (i - prevOccur[num]) + (prevOccur[num] - prevPrevOccur[num]));
+
+                prevPrevOccur[num] = prevOccur[num];
+                prevOccur[num] = i;
+            }
+        }
+
+        return (ans == 1_000_000_000) ? -1 : ans;
+    }
+}
