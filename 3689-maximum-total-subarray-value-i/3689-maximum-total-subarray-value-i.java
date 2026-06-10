e623dfc546038f761513060aa2efa5476e603e9f
Anurag Banerjee <anuragbnrj@gmail.com>
2026-06-09 23:18:18 -0700

    Time: 1 ms (100%), Space: 62.2 MB (65.44%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -1,13 +1,12 @@
 class Solution {
+
     public long maxTotalValue(int[] nums, int k) {
-        int mn = Integer.MAX_VALUE;
-        int mx = Integer.MIN_VALUE;
-
-        for (int num : nums) {
-            mn = Math.min(mn, num);
-            mx = Math.max(mx, num);
+        int m1 = Integer.MAX_VALUE;
+        int m2 = Integer.MIN_VALUE;
+        for (int x : nums) {
+            m1 = Math.min(m1, x);
+            m2 = Math.max(m2, x);
         }
-
-        return 1L * k * (mx - mn);
+        return (long) (m2 - m1) * k;
     }
 }
