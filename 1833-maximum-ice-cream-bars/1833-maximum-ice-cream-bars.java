c52dc3709b567f094576053d959886053be79a59
Anurag Banerjee <anuragbnrj@gmail.com>
2026-06-21 17:03:46 -0700

    Time: 5 ms (92.17%), Space: 81.7 MB (99.24%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,17 +1,26 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
+        int[] freq = new int[100005];
 
-        int bars = 0;
         for (int cost : costs) {
-            if (coins >= cost) {
-                bars += 1;
-                coins -= cost;
-            } else {
-                break;
-            }
+            freq[cost] += 1;
         }
         
+        int bars = 0;
+        for (int i = 1; i < 100005 && coins > 0; i++) {
+            if (freq[i] == 0) continue;
+
+            int possible = coins / i;
+            if (possible < freq[i]) {
+                coins = 0;
+            } else {
+                possible = freq[i];
+                coins = coins - (i * freq[i]);
+            }
+
+            bars += possible;
+        }
+
         return bars;
     }
 }
