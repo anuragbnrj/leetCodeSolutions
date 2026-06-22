4909db2a1b0292f1690bc931c40c0d4b4158d56d
Anurag Banerjee <anuragbnrj@gmail.com>
2026-06-21 19:17:54 -0700

    Time: 2 ms (96.67%), Space: 43.1 MB (87.41%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,23 +1,28 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        int[] textFreq = new int[26];
+        Map<Character, Integer> balloonFreq = new HashMap<>();
+        balloonFreq.put('b', 1);
+        balloonFreq.put('a', 1);
+        balloonFreq.put('l', 2);
+        balloonFreq.put('o', 2);
+        balloonFreq.put('n', 1);
+
+        int[] freq = new int[26];
         for (char ch : text.toCharArray()) {
-            textFreq[ch - 'a'] += 1;
-        }
-        
-        String pattern = "balloon";
-        int[] patternFreq = new int[26];
-        for (char ch : pattern.toCharArray()) {
-            patternFreq[ch - 'a'] += 1;
+            freq[ch - 'a'] += 1;
         }
 
-        int ans = (int) 1e9;
-        for (int i = 0; i < 26; i++) {
-            if (patternFreq[i] != 0) {
-                ans = Math.min(ans, textFreq[i] / patternFreq[i]);
-            }
+        int ans = 100005;
+        for (char ch : balloonFreq.keySet()) {
+            int req = balloonFreq.get(ch);
+
+            int present = freq[ch - 'a'];
+
+            int possible = present / req;
+
+            ans = Math.min(ans, possible);
         }
 
-        return ans;
+        return ans == 100005 ? 0 : ans;
     }
 }
